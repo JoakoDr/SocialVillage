9cd7889c2ba7b91ecde27c2eeddacf757dd26215
JoaquinDiaz <joaco_diaz_ramirez@hotmail.com>
2018-04-24 03:34:46 -0700

    Subir imagenes a Firebase (Solamente en una carpeta)

--- rural/VCSelectImg.swift
+++ rural/VCSelectImg.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import FirebaseStorage
 
 class VCSelectImg: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var imgSub:UIImageView?
         let imagePicker = UIImagePickerController()
+    
     var imgData:Data?
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,10 +38,14 @@ class VCSelectImg: UIViewController, UIImagePickerControllerDelegate, UINavigati
     }
     @IBAction func accionBotonSubir()
     {
+       
         if imgData != nil {
+          
         let imagenRef = DataHolder.sharedInstance.firStorageRef?.child("imagenes/imagen1.jpg")
+            let metadata =  StorageMetadata()
+            metadata.contentType = "image/jpeg"
         // Upload the file to the path "images/rivers.jpg"
-        let uploadTask = imagenRef?.putData(imgData!, metadata: nil) { (metadata, error) in
+        let uploadTask = imagenRef?.putData(imgData!, metadata: metadata) { (metadata, error) in
             guard let metadata = metadata else {
                // print("ERROR!!! ",error)
                 // Uh-oh, an error occurred!
@@ -56,7 +62,7 @@ class VCSelectImg: UIViewController, UIImagePickerControllerDelegate, UINavigati
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let img = info[UIImagePickerControllerOriginalImage] as? UIImage
-        imgData = UIImageJPEGRepresentation(img!, 0.5)!
+        imgData = UIImageJPEGRepresentation(img!, 1)!
         imgSub?.image = img
         self.dismiss(animated: true, completion: nil)
     }
